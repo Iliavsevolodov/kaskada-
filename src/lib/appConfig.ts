c49948bec4165f29410270efa4b4ac4b5b9fe5cf
Iliavsevolodov <78146096+Iliavsevolodov@users.noreply.github.com>
2026-07-08 01:53:37 +0300

    Add shared app config

---
+++ src/lib/appConfig.ts
@@ -0,0 +1,8 @@
+export const appConfig = {
+  name: 'Каскада Маркет',
+  shortName: 'Каскада',
+  description: 'Маркетплейс локальных брендов с системой рекомендаций.',
+  themeColor: '#151515',
+  backgroundColor: '#f7f2e8',
+  basePath: '/kaskada-'
+};
