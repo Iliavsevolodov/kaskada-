60a1ecdc186a8f3044835fe26f7482b0889a1660
Iliavsevolodov <78146096+Iliavsevolodov@users.noreply.github.com>
2026-07-08 02:36:26 +0300

    Bump version for buyer marketplace

--- src/lib/appVersion.ts
+++ src/lib/appVersion.ts
@@ -1,2 +1,2 @@
-export const appVersion = 'v0.2.8';
-export const appVersionName = 'Fashion Marketplace Interface';
+export const appVersion = 'v0.2.9';
+export const appVersionName = 'Buyer Marketplace Storefront';
